99d1b4b9c100d41d5581cfb9839cf7268dd48204
Juan Alvarez <alce@mac.com>
2022-11-03 17:54:29 -0600

    contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,27 +1,48 @@
-import { Element } from 'react-scroll';
-import { Box } from 'theme-ui';
+import {Element} from 'react-scroll';
+import {Box, Text} from 'theme-ui';
 import faces from "../../../public/images/faces.png";
+import {Container} from "../Container";
 
 export default function Contact() {
-    return (
-        <section>
-            <Element name="contact us">
-                <Box
-                    sx={{
-                        mt: 6,
-                        backgroundImage: ['none', 'none', 'none', `url(${faces.src})`],
-                        backgroundRepeat: 'no-repeat',
-                        backgroundPosition: ['center 250px'],
-                        pb: 6,
-                    }}
-                >
+  return (
+    <section>
+      <Element name="contact us">
+        <Box
+          sx={{
+            mt: 6,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: ['center 250px'],
+            pb: 6,
+          }}
+        >
+          <Container>
+            <Box
+              sx={{
+                justifyContent: 'center',
+                maxWidth: 750,
+                m: '0 auto',
+                textAlign: 'center',
+              }}
+            >
+              <Text
+                sx={{ display: 'block', textAlign: 'center', mb: 4 }}
+                variant="h3"
+              >
+                Contact us
+              </Text>
 
-                    <h1>Contact us</h1>
+              <Text sx={{ display: 'block', mb: [2, 2] }}>
+                Do you have any questions, suggestions, or just want to say hi?
+                Please <a href="mailto:hello@versux.app"><strong>contact us.</strong></a>
+              </Text>
+            </Box>
+          </Container>
 
-                </Box>
-            </Element>
+
+        </Box>
+      </Element>
 
 
-        </section>
-    )
+    </section>
+  )
 }
