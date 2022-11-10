cb313b8a9288e1ba4d936ebdd9fbdf97f9097efa
ricka7x <ricky.ramirez89@gmail.com>
2022-11-09 18:01:19 -0600

    fix ssr issue with react-reveal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'theme-ui';
 import { theme } from '../theme';
+import config from 'react-reveal/globals';
+
+config({ ssrFadeout: true });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
